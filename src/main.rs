953adc799c0c32ed424aff95deb3963897f8f84b
Dysthesis <antheoraviel@protonmail.com>
2025-04-12 22:35:27 +1000

    Parse the file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod cli;
 mod output;
 
+use std::fs::read_to_string;
+
 use crate::cli::Args;
+use biblatex::Bibliography;
 use clap::Parser;
 
 fn main() {
     let args = Args::parse();
-    println!("file: {:?}", args.file);
-    println!("output: {:?}", args.output_type);
-    println!("format: {:?}", args.output_format);
+    let src = read_to_string(args.file).unwrap();
+    let biblio = Bibliography::parse(src.as_str()).unwrap();
+    println!("Bibliography: {:?}", biblio);
 }
